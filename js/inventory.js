eccd9a5cbfaddbe13cb7b59302444f9eb8a28c16
kishanhitk <kishansharma1231@gmail.com>
2022-03-22 09:26:46 +0530

    feat: add basic filter

--- js/inventory.js
+++ js/inventory.js
@@ -1,20 +1,11 @@
 const getData = async () => {
     const productGrid = $("#product_grid");
     const resp = await fetch("./assets/inventory.json");
-    const json = await resp.json();
-    const sortBy = $("#sort_by").val();
-
+    const data = await resp.json();
+    const filtered = applyFilter(data);
+    const sorted = applySort(filtered);
     productGrid.empty();
 
-    const isLowToHigh = sortBy === "price_low_high";
-
-    const sorted = json.sort((a, b) => {
-        if (isLowToHigh) {
-            return a.mrp - b.mrp;
-        } else {
-            return b.mrp - a.mrp;
-        }
-    });
     sorted.forEach((element) => {
         const cartQuantity = getCartQuantity(element.id);
         const product = $(`
@@ -49,8 +40,42 @@ const getData = async () => {
     });
 };
 
+const applyFilter = (data) => {
+    let selectedStorages = [];
+    $.each($("input[name='storage-input']:checked"), function () {
+        selectedStorages.push($(this).val());
+    });
+    const filtered = [];
+    data.forEach((product) => {
+        if (
+            selectedStorages.length === 0 ||
+            selectedStorages.includes(product.storage)
+        ) {
+            filtered.push(product);
+        }
+    });
+    return filtered;
+};
+
+const applySort = (data) => {
+    const sortBy = $("#sort_by").val();
+    const sorted = data.sort((a, b) => {
+        switch (sortBy) {
+            case "price_low_high":
+                return a.mrp - b.mrp;
+            case "price_high_low":
+                return b.mrp - a.mrp;
+            case "name_a_z":
+                return a.name.localeCompare(b.name);
+            default:
+                return a.name.localeCompare(b.name);
+        }
+    });
+    return sorted;
+};
 function init() {
     getData();
+    $("#apply-filter-btn").on("click", getData);
     $("#sort_by").on("change", getData);
 }
 
